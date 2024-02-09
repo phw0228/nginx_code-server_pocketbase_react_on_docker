a14cb30da69ace5c8fa635ea9be19561db3690cc
hugh park <phw0228@gmail.com>
2024-02-09 06:38:15 +0000

    make Docker-compose

---
+++ react_app_practice/src/Conf.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Conf = () => {
+  return(
+  <div>
+    
+  </div>
+  );
+};
+
+export default Conf;
---
+++ react_app_practice/src/Paper.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from "react";
+import PaperBoard from "./PaperBoard";
+
+const Paper = () => {
+  return (
+    <div>
+      <PaperBoard />
+    </div>
+  );
+};
+
+export default Paper;
